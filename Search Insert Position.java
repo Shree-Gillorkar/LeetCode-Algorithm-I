190d9562d2309df76bcfc7fcd4c96eaf2f472582
Shree Gillorkar <70030205+Shree-Gillorkar@users.noreply.github.com>
2021-09-20 19:26:15 +0530

    Create Search Insert Position.java
    Time Complexity is O(log(n)).

---
+++ Search Insert Position.java
@@ -0,0 +1,54 @@
+/*Given a sorted array of distinct integers and a target value, return the index if the target is found. If not, return the index where it would be if it were inserted in order.
+You must write an algorithm with O(log n) runtime complexity.
+
+Example 1:
+Input: nums = [1,3,5,6], target = 5
+Output: 2
+
+Example 2:
+Input: nums = [1,3,5,6], target = 2
+Output: 1
+
+Example 3:
+Input: nums = [1,3,5,6], target = 7
+Output: 4
+
+Example 4:
+Input: nums = [1,3,5,6], target = 0
+Output: 0
+
+Example 5:
+Input: nums = [1], target = 0
+Output: 0
+ 
+
+Constraints:
+1 <= nums.length <= 104
+-104 <= nums[i] <= 104
+nums contains distinct values sorted in ascending order.
+-104 <= target <= 104
+
+*/
+class Solution {
+    public int searchInsert(int[] nums, int target) {
+        int first = 0; 
+        int last = nums.length-1;
+        int mid =0; 
+        int flag =0;
+        while(last>=first){
+            mid = first + (last-first)/2;
+            if(nums[mid]==target){
+                flag = 1;
+                break;
+            }else if(target>nums[mid]){
+                first = mid+1;
+            }else{
+                last = mid -1;
+            }
+        }
+        if(flag ==0 )
+            return first;
+        else
+            return mid;
+    }
+}
